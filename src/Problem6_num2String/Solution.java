7ef2872606d62e33f5d6c5fa130cee5f3efae329
GoMyeongho <sdx02013@ajou.ac.kr>
2024-09-26 16:44:08 +0900

    6번 완

--- src/Problem6_num2String/Solution.java
+++ src/Problem6_num2String/Solution.java
@@ -1,8 +1,14 @@
 package Problem6_num2String;
 
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) {
-        String[] Area = {"","서울","경기","인천","수원"};
-
+        String[] areas = {"","서울","경기","인천","수원"};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("[1]서울, [2]경기, [3]인천, [4]수원");
+        System.out.print("지역을 입력 : ");
+        int area = sc.nextInt();
+        System.out.println(areas[area]);
     }
 }
