5d84553064364d9b4809b92de76d4903e2ffdbc3
GoMyeongho <sdx02013@ajou.ac.kr>
2024-09-27 17:04:19 +0900

    15번 완

---
+++ src/Problem15_Alarm/Solution15.java
@@ -0,0 +1,14 @@
+package Problem15_Alarm;
+
+import java.util.Scanner;
+
+public class Solution15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hour = sc.nextInt();
+        int min = sc.nextInt();
+        hour = (hour - 1 + min / 45) % 24;
+        min = (min + 15) % 60;
+        System.out.println(hour + ":" + min);
+    }
+}
