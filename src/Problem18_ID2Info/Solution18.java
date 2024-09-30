f557017753d62e87c7f77b603f1d3ab61e2fd420
GoMyeongho <sdx02013@ajou.ac.kr>
2024-09-30 09:51:26 +0900

    18번 완료

--- src/Problem18_ID2Info/Solution18.java
+++ src/Problem18_ID2Info/Solution18.java
@@ -1,5 +1,7 @@
 package Problem18_ID2Info;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Solution18 {
@@ -7,6 +9,20 @@ public class Solution18 {
         Scanner sc = new Scanner(System.in);
         String[] id = sc.nextLine().split("-");
         int birth = Integer.parseInt(id[0]);
-        int gender = Integer.parseInt(id[1])/1000000;
+        int year = birth / 10000;
+        int month = birth % 10000 / 100;
+        int day = birth % 100;
+        int id2 = Integer.parseInt(id[1])/1000000;
+        int realYear = 1900 + year + (id2 - 1 )/ 2 * 100;
+        Date now = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("MMdd");
+        int nowYear = Integer.parseInt(sdf.format(now));
+        int nowDate = Integer.parseInt(sdf2.format(now));
+        System.out.printf("생년 월일 : %d년 %d월 %d일\n", realYear, month, day);
+        if (id2 % 2 == 0) System.out.println("성별 : 여성");
+        else System.out.println("성별 : 남성");
+        if (nowDate > month * 100 + day) System.out.println(nowYear - realYear );
+        else System.out.println(nowYear - realYear - 1);
     }
 }
