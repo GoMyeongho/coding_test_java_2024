ce5b702410595fa18deb2a67b9be7afd75d6e951
GoMyeongho <sdx02013@ajou.ac.kr>
2024-10-02 17:49:25 +0900

    39번 완료

---
+++ src/Problem40_Sonic/Solution40.java
@@ -0,0 +1,15 @@
+package Problem40_Sonic;
+
+import java.util.Scanner;
+
+public class Solution40 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int a = sc.nextInt();
+         int b = sc.nextInt();
+         int c = sc.nextInt();
+         int profit = c - b;
+         if (profit <= 0) System.out.println(-1);
+         else
+    }
+}
